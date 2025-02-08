408d0f4aa1d1abde2053da3e8b9af7dbe975ee3b
KimarenNaidoo <kimarennaidoo@gmail.com>
2025-02-08 11:00:21 +0200

    feat(testing): Add the unit test for the getBookById endpoint

--- src/test/java/com/example/BookCatalogueSpringBootWebApp/BookControllerTest.java
+++ src/test/java/com/example/BookCatalogueSpringBootWebApp/BookControllerTest.java
@@ -21,6 +21,9 @@ import static org.hamcrest.Matchers.containsString;
 @TestInstance(Lifecycle.PER_CLASS)
 public class BookControllerTest {
 
+    private final String apiEndpoint = "/api/books/";
+    private final String getBooksEndpointResponseString = "<h3>Book Id: 1</h3> <ul><li>Title: The Alchemist.</li> <li>Author: Paulo Coelho.</li> <li>Category: Fiction.</li> <li>Status: Finished.</li> <li>Ownership: Unown.</li></ul>";
+
     @Autowired
     private MockMvc mockMvc;
 
@@ -29,15 +32,18 @@ public class BookControllerTest {
 
     @Test
     public void testGetAllBooksEndpoint() throws Exception {
-        mockMvc.perform(get("/api/books/")
+        mockMvc.perform(get(apiEndpoint)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("<h3>Book Id: 1</h3> <ul><li>Title: The Alchemist.</li> <li>Author: Paulo Coelho.</li> <li>Category: Fiction.</li> <li>Status: Finished.</li> <li>Ownership: Unown.</li></ul>")));
+                .andExpect(content().string(containsString(getBooksEndpointResponseString)));
     }
 
     @Test 
-    public void testGetBookByIdEnpoint() throws Exception {
-        
+    public void testGetBookByIdEndpoint() throws Exception {
+        mockMvc.perform(get(apiEndpoint + "{id}", "1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(getBooksEndpointResponseString));
     }
 
 }
