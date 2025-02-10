962044a67d4844c21d76de10a5a11ce5cea6b003
KimarenNaidoo <kimarennaidoo@gmail.com>
2025-02-10 18:32:09 +0200

    feat: Added a book repository that will serve as loading the resources - Updated the sprinboot version from 3.4.1 to 3.4.2

--- src/main/java/com/example/BookCatalogueSpringBootWebApp/service/BookService.java
+++ src/main/java/com/example/BookCatalogueSpringBootWebApp/service/BookService.java
@@ -1,44 +1,22 @@
 package com.example.BookCatalogueSpringBootWebApp.service;
 
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StreamUtils;
 
 import com.example.BookCatalogueSpringBootWebApp.model.Book;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.example.BookCatalogueSpringBootWebApp.repository.BookRepository;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;  
 
 @Service
 public class BookService {
 
     private List<Book> books;
-    private final ObjectMapper objectMapper;
+    private final BookRepository bookRepository;
 
     public BookService() {
-        this.objectMapper = new ObjectMapper();
-        loadBooksFromJson();
-    }
-
-    private void loadBooksFromJson() {
-        try {
-            ClassPathResource resource = new ClassPathResource("booksData.json");
-            String jsonContent = StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
-            Map<String, List<Book>> booksMap = objectMapper.readValue(jsonContent, new TypeReference<Map<String, List<Book>>>() {}
-            );
-            
-            this.books = booksMap.get("books");
-        }
-        catch (IOException e) {
-            this.books = new ArrayList<>();
-            e.printStackTrace();
-        }
+        bookRepository = new BookRepository();
+        this.books = bookRepository.loadBooksFromRepository();
     }
 
     public List<Book> getAllBooks() {
